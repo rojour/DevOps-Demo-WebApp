3997b3970fbc04fdfea0baeee42072ad06a652ec
root <root@ip-172-31-55-208.ec2.internal>
2021-02-21 16:28:06 +0000

    final change before finish program

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://54.83.175.164:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.146.85.202:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
